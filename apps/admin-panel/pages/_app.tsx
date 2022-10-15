950e13dd5b0a111042be1ba9f1eb5f3af3deb599
Elmer van Rooijen <e.van.rooijen@hotmail.com>
2022-10-15 21:37:21 +0200

    chore: move darkmode logic to component

--- apps/admin-panel/pages/_app.tsx
+++ apps/admin-panel/pages/_app.tsx
@@ -2,12 +2,12 @@ import {
   DehydratedState,
   Hydrate,
   QueryClient,
-  QueryClientProvider,
+  QueryClientProvider
 } from '@tanstack/react-query';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import DarkModeContext, { DarkModeType } from '../components/DarkModeContext';
+import { useState } from 'react';
+import { DarkMode } from '../components/DarkMode';
 
 import '../styles/tailwind.css';
 
@@ -18,45 +18,15 @@ type BasePageProps = {
 function CustomApp({ Component, pageProps }: AppProps<BasePageProps>) {
   const [queryClient] = useState(() => new QueryClient());
 
-  const isBrowserDefaultDark = () => {
-    if (typeof window === 'undefined') {
-      return false;
-    }
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ?? false;
-  };
-
-  const [theme, setTheme] = useState<DarkModeType>('dark');
-
-  useEffect(() => {
-    const storedTheme = window.sessionStorage.getItem(
-      'vortex_theme'
-    ) as DarkModeType;
-    if (storedTheme) {
-      setTheme(storedTheme);
-    } else {
-      setTheme(isBrowserDefaultDark() ? 'dark' : 'light');
-    }
-  }, []);
-
-  useEffect(() => {
-    if (theme === 'dark') {
-      window.sessionStorage.setItem('vortex_theme', 'dark');
-      document.documentElement.classList.add('dark');
-    } else {
-      window.sessionStorage.setItem('vortex_theme', 'light');
-      document.documentElement.classList.remove('dark');
-    }
-  }, [theme]);
-
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <DarkModeContext.Provider value={{ theme, setTheme }}>
+        <DarkMode>
           <Head>
             <title>Welcome to admin-panel!</title>
           </Head>
           <Component {...pageProps} />
-        </DarkModeContext.Provider>
+        </DarkMode>
       </Hydrate>
     </QueryClientProvider>
   );
